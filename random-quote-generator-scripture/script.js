1122d478fec3fbe0a6e76727481c57dc3c77871c
MahenM1971 <mmahindaratne88@gmail.com>
2025-04-26 23:16:53 +0530

    Update script.js

--- random-quote-generator-scripture/script.js
+++ random-quote-generator-scripture/script.js
@@ -1,15 +1,17 @@
 const quoteText = document.querySelector(".quote"),
       authorName = document.querySelector(".name");
 
-// Array of scripture verses
+// Array of scripture verses and their display times
 const quotes = [
   {
     content: "There were three who always walked with the Lord: Mary his mother, her sister, and Magdalene, the one who was called his companion. For 'Mary' is the name of his sister, his mother, and his companion.",
-    author: "Gospel according to Philip 63:32-64:5"
+    author: "Gospel according to Philip 63:32-64:5",
+    time: 10000 // 10 seconds
   },
   {
     content: "And the companion of the Savior is Mary Magdalene. But Christ loved her more than all the disciples and used to kiss her often on her mouth. The rest of the disciples were offended by it and expressed disapproval. They said to him, 'Why do you love her more than all of us?'",
-    author: "Gospel according to Philip 55:13-14"
+    author: "Gospel according to Philip 55:13-14",
+    time: 20000 // 20 seconds
   },
   {
     content: `When Mary had said this, she fell silent, since it was to this point that the Savior had spoken with her. 
@@ -23,19 +25,24 @@ Then Mary wept and said to Peter, "My brother Peter, what do you think? Do you t
 
 Levi answered and said to Peter, "Peter, you have always been hot-tempered. Now I see you contending against the woman like the adversaries. 
 But if the Savior made her worthy, who are you indeed to reject her? Surely the Savior knew her very well. That is why he loved her more than us."`,
-    author: "Gospel according to Mary Magdalene 9:4-10:14"
+    author: "Gospel according to Mary Magdalene 9:4-10:14",
+    time: 40000 // 40 seconds
   }
 ];
 
-function randomQuote() {
-  const randomIndex = Math.floor(Math.random() * quotes.length);
-  const quote = quotes[randomIndex];
+let currentIndex = 0;
+
+function showQuote() {
+  const quote = quotes[currentIndex];
   quoteText.innerText = quote.content;
   authorName.innerText = quote.author;
+
+  // Set up the next quote after its specific time
+  setTimeout(() => {
+    currentIndex = (currentIndex + 1) % quotes.length; // cycle through quotes
+    showQuote(); // show the next one
+  }, quote.time);
 }
 
-// First load
-randomQuote();
-
-// Refresh every 60 seconds (60,000ms)
-setInterval(randomQuote, 60000);
+// Start
+showQuote();
